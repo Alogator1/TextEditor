0415c3685de3a0f09883f35a7ea963fa58e98468
unknown <vtelyachy@gmail.com>
2020-09-16 02:05:33 +0300

    Updates

---
+++ precache-manifest.b02fce504e89c924b4816e539fbec317.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59fdffa48549eb8270fe77fa1454917d",
+    "url": "/TextEditor/index.html"
+  },
+  {
+    "revision": "e8259ef4db848a7b019b",
+    "url": "/TextEditor/static/css/main.54c7fc54.chunk.css"
+  },
+  {
+    "revision": "0966f1a85d46598c63d5",
+    "url": "/TextEditor/static/js/2.b6ba367e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/TextEditor/static/js/2.b6ba367e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8259ef4db848a7b019b",
+    "url": "/TextEditor/static/js/main.a98957d9.chunk.js"
+  },
+  {
+    "revision": "dd34e0f90cf978cc00ea",
+    "url": "/TextEditor/static/js/runtime-main.05ba527d.js"
+  }
+]);
